1d63569817d3554ed3f7d767fbf14ed1368fd807
NasipCOM <salmahunov@mail.ru>
2019-07-09 17:53:47 +0600

    Finished checkbox and Team section

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
     $(".multiple-items").slick({
       infinite: true,
       dots: true,
-      slidesToShow: 5,
-      slidesToScroll: 5,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       adaptiveHeight: true,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToShow: 2,
+            slidesToScroll: 2   ,
             infinite: true,
             dots: true
           }
@@ -18,8 +18,8 @@ $(document).ready(function() {
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
           }
         },
         {
